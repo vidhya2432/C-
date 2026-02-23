83a16a2664d864e176cd4e8ed9e78a846cde04f5
Vidhya-2024-aib <vidhyamani24032007@gmail.com>
2026-02-23 08:22:14 +0530

    Eligible to Voting

---
+++ Eligible to Voting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age > 18) {
+        cout << "You are eligible for voting;
+    }
+    else if(age < 18) {
+        cout << "You are not eligible for voting ;
+    }
+    else if(age == 18) {
+        cout << "Apply for Voter ID";
+    }
+
+    return 0;
+}
